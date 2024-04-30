5ad86229bff0feeacf6e3cc60e48d4889283506c
PrasannaAdabala <prasannaadabala385@gmail.com>
2024-04-30 17:23:21 +0530

    file

---
+++ index.js
@@ -0,0 +1,107 @@
+Program to print 1 to num, num to 1, -1 to -num, -num to -1 by using while loop
+// const input=require("prompt-sync")();
+// let i=input("enter a number=") 
+// let num=10;
+// while(!(i>num)){
+//     console.log(i)
+//     i++
+
+// }
+// const input=require("prompt-sync")();
+// let i=input("enter a number=") 
+// let num=1;
+// while(!(num>i)){
+//     console.log(i)
+//     i--
+
+// }
+// const input=require("prompt-sync")();
+// let i=input("enter a number=") 
+// let num=-10;
+// while((i>=num)){
+//     console.log(i)
+//     i--
+
+// }
+// const input=require("prompt-sync")();
+// let i=input("enter a number=") 
+// let num=-1;
+// while((num>=i)){
+//     console.log(i)
+//     i++
+
+// }
+Write a program to find the largest prime in the given number
+// input: 46537
+// output: 7/
+// const a=require("prompt-sync")();
+// let num=a("enter a number=") 
+// max = 0;
+// for (n of num) {
+//     if (n > 1) {
+//         c = true;
+//         for (i = 2; i < n; i++) {
+//             if (n % i == 0) {
+//                 c = false;
+//                 break;
+//             }
+//         }
+//         if (c) {
+//             if (max < n)
+//                 max = n;
+//         }
+//     }
+// }
+// console.log(max);
+
+// Write a program to print the sum of largest and smallest prime digits in the given number
+// input: 5497351
+// output: 10
+// const a=require("prompt-sync")();
+// let num=a("enter a number=") 
+
+// max = 0;
+// min=num[0]
+// for (n of num) {
+//     if (n > 1) {
+//         c = true;
+//         for (i = 2; i < n; i++) {
+//             if (n % i == 0) {
+//                 c = false;
+//                 break;
+//             }
+//         }
+//         if (c) {
+//             if (max < n)
+//                 max = n;
+//             if(min>n)
+//             min=n
+//         }
+//     }
+// }
+// console.log(Number(max)+Number(min));
+
+// 4) Write a program to print the largest non prime digit in the given number
+// input: 463482
+// output: 8
+// const a=require("prompt-sync")();
+// let num=a("enter a number=") 
+// max = 0;
+// for (n of num) {
+//     if (n > 1) {
+//         c = true;
+//         for (i = 2; i < n; i++) {
+//             if (n % i == 0) {
+//                 if(max<n)
+//                 max=n;
+//                 c = false;
+//                 break;
+//             }
+//         }
+//     }
+// }
+// console.log(max);
+
+
+
+
